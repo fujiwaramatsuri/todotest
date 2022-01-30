0797dd615c375b837d6dfe675ac2df157aa9f6ce
fujiwaramatsuri <92214720+fujiwaramatsuri@users.noreply.github.com>
2022-01-30 18:15:17 +0900

    Add files via upload

---
+++ 2022_01_26_030638_create_todo__table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todo', function (Blueprint $table) {
+            $table->id('BIGINT',20);
+            $table->string('content',191);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todo');
+    }
+}
