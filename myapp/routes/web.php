495cf2d5209f290d5bf3d91a96445f7f4dfe0658
fujiwaramatsuri <92214720+fujiwaramatsuri@users.noreply.github.com>
2022-02-06 14:55:05 +0900

    Add files via upload

--- myapp/routes/web.php
+++ myapp/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
 
+Route::get('/',TodoController::class,'index');
+/*Route::post('/todo/create',TodoController::class,'create');
+Route::post('/todo/update',TodoController::class,'update');
+Route::post('/todo/delete',TodoController::class,'delete');
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
+*/
